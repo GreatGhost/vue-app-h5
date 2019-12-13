86000d22ea3669131da6fd7ca0a22afbbfca8540
GreatGhost <1609401009@qq.com>
2019-12-13 15:55:08 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<mt-button size="small">small</mt-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { Button } from 'mint-ui';
+import Vue from 'vue';
+Vue.component(Button.name, Button);
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import './config/rem';
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 Vue.config.productionTip = false
+
+// 按需引入mint-ui
+Vue.use(MintUI)
+
 
 new Vue({
   render: h => h(App),
