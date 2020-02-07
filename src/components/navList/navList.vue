0ff94ada6b003a404f5c5695293c23f1f90691ae
GreatGhost <1609401009@qq.com>
2020-02-07 22:10:55 +0800

    1.提交新的代码

--- src/components/navList/navList.vue
+++ src/components/navList/navList.vue
@@ -1,60 +1,72 @@
 <template>
-  <div class="navList">
-      <ul class="list">
-          <li class="nav-list-item" :class="[item.isSelect?'active':'']" v-for="(item,index) in list" :key="index">{{item.name}}</li>
-      </ul>
-  </div>
+    <div class="navList">
+        <ul class="list">
+            <li class="nav-list-item" :class="[item.isSelect?'active':'']" v-for="(item,index) in list" :key="index"
+                @click="select(index)">{{item.name}}</li>
+        </ul>
+    </div>
 </template>
 
 <script>
-export default {
-  components:{},
-  props:{
-      list:{
-          type:Array,
-          default(){
-              return []
-          }
-      }
-  },
-  data(){
-    return {
+    export default {
+        components: {},
+        props: {
+            list: {
+                type: Array,
+                default () {
+                    return []
+                }
+            }
+        },
+        data() {
+            return {}
+        },
+        watch: {},
+        computed: {},
+        methods: {
+            select(index) {
+                this.$emit('select', index)
+            }
+
+        },
+        created() {},
+        mounted() {}
     }
-  },
-  watch:{},
-  computed:{},
-  methods:{},
-  created(){},
-  mounted(){}
-}
 </script>
 <style lang="less" scoped>
-.list{
-    display: flex;
-    align-items: center;
-    li{
-       position: relative;
-       flex:1;
-       height:.89rem;
-       line-height:.89rem;
-       font-size: .32rem;
-       list-style-type:none;
-       text-align: center;
-       font-weight: 700;
-       box-sizing:border-box;
+    .list {
+        display: flex;
+        align-items: center;
+        border-bottom:1px solid rgb(228, 230, 240);
+        li {
+            position: relative;
+            flex: 1;
+            height: .89rem;
+            line-height: .89rem;
+            font-size: .32rem;
+            list-style-type: none;
+            text-align: center;
+            font-weight: 700;
+            box-sizing: border-box;
+            color: rgb(147, 153, 165);
+        }
+
+        li.active {
+            color: #000;
+        }
+
+        li.active::after {
+            content: '';
+            display: block;
+            position: absolute;
+            bottom: .08rem;
+            left: 50%;
+            height: .06rem;
+            width: .32rem;
+            background: #3072F6;
+            border: .06rem;
+            transform: translateX(-50%);
+            border-radius: .03rem;
+        }
     }
-    li.active::after{
-        content:'';
-        display:block;
-        position: absolute;
-        bottom: 0;
-        left:50%;
-        height:.06rem;
-        width:.32rem;
-        background:#3072F6;
-        border:.06rem;
-        transform:translateX(-50%);
-        border-radius:.03rem;
-    }
-}
 </style>
--- src/pages/selectCity/selectCity.vue
+++ src/pages/selectCity/selectCity.vue
@@ -1,24 +1,29 @@
 <template>
     <div class="selectCity">
-        <CityHeaderSearch />
-        <navList :list="nav" />
+        <div class="search-wrapper">
+            <img src="https://s1.ljcdn.com/mensa/static/m/images/common/close@2x.png?version=fkck1hud4" alt=""
+                class="clear">
+            <div class="search">
+                请输入城市名称
+            </div>
+        </div>
+        <navList :list="nav" @select="tapNav" />
     </div>
 </template>
 
 <script>
-import CityHeaderSearch from '../../components/CityHeaderSearch/CityHeaderSearch'
-import navList from '../../components/navList/navList'
+    import CityHeaderSearch from '../../components/CityHeaderSearch/CityHeaderSearch'
+    import navList from '../../components/navList/navList'
 
     export default {
         components: {
             CityHeaderSearch: CityHeaderSearch,
-            navList:navList
+            navList: navList
         },
         props: {},
         data() {
             return {
-                nav: [
-                    {
+                nav: [{
                         name: '国内城市',
                         isSelect: true
                     },
@@ -31,11 +36,59 @@ import navList from '../../components/navList/navList'
         },
         watch: {},
         computed: {},
-        methods: {},
+        methods: {
+            tapNav(idx) {
+
+                let nav = this.nav;
+                nav.forEach((tmp, index) => {
+                    if (idx === index) {
+                        tmp.isSelect = true;
+                    } else {
+                        tmp.isSelect = false;
+                    }
+                })
+            },
+
+            getCurrentCity() {
+                if (navigator.geolocation) {
+                    var n = navigator.geolocation.getCurrentPosition(function (res) {
+                        console.log(res); // 需要的坐标地址就在res中
+                    });
+                } else {
+                    alert('该浏览器不支持定位');
+                }
+            }
+        },
         created() {},
-        mounted() {}
+        mounted() {
+            this.getCurrentCity();
+        }
     }
 </script>
 <style lang="less" scoped>
-    .selectCity {}
+    .selectCity {
+        .search-wrapper {
+            padding: .12rem .3rem;
+            background: #fff;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            img {
+                width: .48rem;
+                height: .48rem;
+                margin-right: .4rem;
+            }
+
+            .search {
+                font-size: .24rem;
+                height: .74rem;
+                flex: 1;
+                border-radius: .37rem;
+                background: rgb(248, 248, 249);
+                line-height: .74rem;
+                padding: 0 .12rem 0 .64rem;
+            }
+        }
+    }
 </style>
